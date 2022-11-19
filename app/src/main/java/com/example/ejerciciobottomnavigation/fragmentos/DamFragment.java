99fcdcd1a99a173456cff75200841a879921db5b
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2022-11-19 20:13:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ejerciciobottomnavigation/fragmentos/DamFragment.java
@@ -0,0 +1,21 @@
+package com.example.ejerciciobottomnavigation.fragmentos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ejerciciobottomnavigation.R;
+
+public class DamFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dam, container, false);
+    }
+}
